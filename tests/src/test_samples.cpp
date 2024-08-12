2829e3977fa525430c879ed87e8f3e79dcb1b868
Lukasz Kazmierczak <lukask84@o2.pl>
2024-08-12 19:40:28 +0200

    Updating unit tests
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- tests/src/test_samples.cpp
+++ tests/src/test_samples.cpp
@@ -5,15 +5,8 @@
 
 #include "gtest/gtest.h"
 
-class TestSamples : public testing::Test
+class TestSample : public testing::Test
 {
-  public:
-    std::shared_ptr<Sample> sample;
-    std::shared_ptr<std::vector<NotifyFunc>> notifiers{
-        new std::vector<NotifyFunc>{{[this](const SampleData& data) {
-            notifiedvalue = std::get<double>(data);
-        }}}};
-
   protected:
     const int32_t angle{30};
     const double validdistance{55.f};
@@ -21,69 +14,40 @@ class TestSamples : public testing::Test
     double notifiedvalue{};
 
     void SetUp() override
-    {
-        sample = std::make_shared<Sample>(angle, notifiers);
-    }
+    {}
 
     void TearDown() override
     {}
+
+    SampleData getData(double distance)
+    {
+        return {this->angle, distance};
+    }
 };
 
-TEST_F(TestSamples, IsValidAfterValidValueUpdate)
+TEST_F(TestSample, IsValidCreatedWithValidDistance)
 {
-    auto value = validdistance;
-    sample->update(value);
-    EXPECT_TRUE(sample->isvalid());
+    Sample sample{angle, validdistance};
+    EXPECT_TRUE(sample.isvalid());
 }
 
-TEST_F(TestSamples, IsInvalidAfterInvalidValueUpdate)
+TEST_F(TestSample, IsValidCreatedWithInvalidDistance)
 {
-    auto value = invaliddistance;
-    sample->update(value);
-    EXPECT_FALSE(sample->isvalid());
+    Sample sample{angle, invaliddistance};
+    EXPECT_FALSE(sample.isvalid());
 }
 
-TEST_F(TestSamples, IsSingleNotifiedAfterValidValueUpdate)
+TEST_F(TestSample, IsGetDataProperWithValidDistance)
 {
-    auto value = validdistance;
-    sample->update(value);
-    EXPECT_EQ(value, notifiedvalue);
+    Sample sample{angle, validdistance};
+    EXPECT_EQ(sample.get(), SampleData(angle, validdistance));
 }
 
-TEST_F(TestSamples, IsTwiceNotifiedAfterValidValueUpdate)
+TEST_F(TestSample, IsGetDataProperWithInvalidDistance)
 {
-    double secondnotifiedvalue{};
-    notifiers->push_back([&secondnotifiedvalue](auto& data) {
-        secondnotifiedvalue = std::get<double>(data);
-    });
-    auto value = validdistance;
-    sample->update(value);
-    ASSERT_NE(value, double{});
-    EXPECT_EQ(value, notifiedvalue);
-    EXPECT_EQ(value, secondnotifiedvalue);
-}
-
-TEST_F(TestSamples, IsNotNotifiedAfterInvalidValueUpdate)
-{
-    auto value = invaliddistance;
-    sample->update(value);
-    ASSERT_NE(value, notifiedvalue);
-    EXPECT_EQ(notifiedvalue, double{});
-}
-
-TEST_F(TestSamples, IsInvalidAfterValidValueUpdate)
-{
-    auto value = invaliddistance;
-    sample->update(value);
-    ASSERT_FALSE(std::isnan(value));
-    EXPECT_TRUE(std::isnan(std::get<double>(sample->get())));
-}
-
-TEST_F(TestSamples, IsAfterValidValueUpdateProperlyReset)
-{
-    auto value = validdistance;
-    sample->update(value);
-    ASSERT_TRUE(sample->isvalid());
-    sample->reset();
-    EXPECT_FALSE(sample->isvalid());
+    Sample sample{angle, invaliddistance};
+    auto data = sample.get();
+    ASSERT_NE(data, SampleData(angle, invaliddistance));
+    EXPECT_EQ(data.first, angle);
+    EXPECT_TRUE(std::isnan(data.second));
 }

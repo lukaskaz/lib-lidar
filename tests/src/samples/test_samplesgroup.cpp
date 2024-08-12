809cdf3948b3d0a8f901ddfe53c7e32284d4517d
Lukasz Kazmierczak <lukask84@o2.pl>
2024-08-12 23:17:28 +0200

    Unit tests are aligned, not yet completed though
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

---
+++ tests/src/samples/test_samplesgroup.cpp
@@ -0,0 +1,122 @@
+#include "samples.hpp"
+
+#include <memory>
+
+#include "gtest/gtest.h"
+
+class TestSamplesGroup : public testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+        samplesgroup = std::make_shared<SamplesGroup>(angle);
+        samplesgroup->addnotifier(std::move(defaultnotifier));
+    }
+
+    void TearDown() override
+    {}
+
+  protected:
+    std::shared_ptr<SamplesGroup> samplesgroup;
+    const int32_t angle{30};
+    const double validdistance{55.f};
+    const double invaliddistance{.5f};
+    const SampleData validdata{angle, validdistance};
+    const SampleData invaliddata{angle, invaliddistance};
+    int32_t notifycounter{};
+    SampleData notifieddata{};
+
+    NotifyFunc defaultnotifier{[this](auto& data) {
+        notifycounter++;
+        notifieddata = data;
+    }};
+};
+
+TEST_F(TestSamplesGroup, IsDefaultAnglesProperlyCreated)
+{
+    std::vector<int32_t> expectedangles = {
+        angle - 1,
+        angle,
+        angle + 1,
+    };
+    EXPECT_EQ(expectedangles, samplesgroup->getangles());
+}
+
+TEST_F(TestSamplesGroup, AreValidSamplesProperlyOnceNotified)
+{
+    SampleData datasuppprev(validdata.first - 1, validdata.second + 1);
+    SampleData datasuppnext(validdata.first + 1, validdata.second + 2);
+    samplesgroup->addsampletonotify(datasuppnext);
+    samplesgroup->addsampletonotify(validdata);
+    samplesgroup->addsampletonotify(datasuppprev);
+    ASSERT_NE(notifycounter, 1);
+    samplesgroup->notifyandcleanup();
+    EXPECT_EQ(notifycounter, 1);
+}
+
+TEST_F(TestSamplesGroup, AreInvalidSamplesProperlyIgnored)
+{
+    SampleData datasuppprev(invaliddata.first - 1, invaliddata.second);
+    SampleData datasuppnext(invaliddata.first + 1, invaliddata.second);
+    samplesgroup->addsampletonotify(datasuppnext);
+    samplesgroup->addsampletonotify(invaliddata);
+    samplesgroup->addsampletonotify(datasuppprev);
+    samplesgroup->notifyandcleanup();
+    EXPECT_EQ(notifycounter, 0);
+}
+
+TEST_F(TestSamplesGroup, IsHighestPrioMainSampleProperlyNotified)
+{
+    SampleData datasuppprev(validdata.first - 1, validdata.second + 1);
+    SampleData datasuppnext(validdata.first + 1, validdata.second + 2);
+    samplesgroup->addsampletonotify(datasuppnext);
+    samplesgroup->addsampletonotify(datasuppprev);
+    samplesgroup->addsampletonotify(validdata);
+    ASSERT_NE(notifieddata, validdata);
+    samplesgroup->notifyandcleanup();
+    EXPECT_EQ(notifieddata, validdata);
+}
+
+TEST_F(TestSamplesGroup, IsHighestPrioFirstSuppSampleProperlyNotified)
+{
+    SampleData datasuppprev(validdata.first - 1, validdata.second + 1);
+    SampleData datasuppnext(validdata.first + 1, validdata.second + 2);
+    samplesgroup->addsampletonotify(datasuppnext);
+    samplesgroup->addsampletonotify(datasuppprev);
+    ASSERT_NE(notifieddata, datasuppprev);
+    samplesgroup->notifyandcleanup();
+    EXPECT_EQ(notifieddata, datasuppprev);
+}
+
+TEST_F(TestSamplesGroup, IsHighestPrioSecondSuppSampleProperlyNotified)
+{
+    SampleData datasuppnext(validdata.first + 1, validdata.second + 5);
+    samplesgroup->addsampletonotify(datasuppnext);
+    ASSERT_NE(notifieddata, datasuppnext);
+    samplesgroup->notifyandcleanup();
+    EXPECT_EQ(notifieddata, datasuppnext);
+}
+
+TEST_F(TestSamplesGroup, AreSeveralNotifiersProperlyCalled)
+{
+    int32_t notifycountersecond{}, notifycounterthird{};
+    samplesgroup->addnotifier(
+        [&notifycountersecond]([[maybe_unused]] auto& data) {
+            notifycountersecond++;
+        });
+    samplesgroup->addnotifier(
+        [&notifycounterthird]([[maybe_unused]] auto& data) {
+            notifycounterthird++;
+        });
+
+    samplesgroup->addsampletonotify(validdata);
+    auto initial =
+        std::make_tuple(notifycounter, notifycountersecond, notifycounterthird);
+    samplesgroup->notifyandcleanup();
+
+    auto expected = std::make_tuple(1, 1, 1);
+    auto received =
+        std::make_tuple(notifycounter, notifycountersecond, notifycounterthird);
+    ASSERT_NE(expected, initial);
+    EXPECT_EQ(expected, received);
+}
